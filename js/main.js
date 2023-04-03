135f02e32a91bb12150da42016811d4f3de9c746
lenara k <lkrocket800@gmail.com>
2023-04-04 00:35:22 +0300

    переименовывает data.js в mock-data.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import { createPhotos} from './data.js';
+import { createPhotos} from './mock-data.js';
 import { renderThumbnails } from './thumbnails.js';
 import { addFullImageFeatureToPictures } from './full-image.js';
 import './image-upload-form.js';
 import './scale.js';
 import './effects.js';
 
-const photos = createPhotos();
-renderThumbnails(photos);
-addFullImageFeatureToPictures(photos);
+const mockPhotos = createPhotos();
+renderThumbnails(mockPhotos);
+addFullImageFeatureToPictures(mockPhotos);
